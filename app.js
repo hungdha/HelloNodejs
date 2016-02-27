265fc9d857ae3fd59bd9a2e8d134429b4e8ea5a6
Le Hung <tuanhung.cntt@gmail.com>
2016-02-27 13:46:38 +0700

    commit 2

---
+++ app.js
@@ -0,0 +1,7 @@
+// require lib
+var express = require('express');
+var path = require("path");
+var app = express();
+// view engine setup
+app.set("views", path.join(__dirname, "views" ));
+app.set("view engine","jade");
